4058a911aa594590e77a8769cedfa10534cda74c
Celina Lind <lind.115@buckeyemail.osu.edu>
2021-09-27 22:14:52 -0400

    update license text returned for MIT

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -35,7 +35,28 @@ function renderLicenseSection(license) {
       section = 'This project has a license with [Apache License Version 2.0], January 2004 (http://www.apache.org/licenses/)' ; 
       break;
     case 'MIT License':
-      section = 'This project has a license with [MIT License], (https://choosealicense.com/licenses/mit/)' ; 
+      section = `
+      MIT License
+
+      Copyright (c) [year] [fullname]
+      
+      Permission is hereby granted, free of charge, to any person obtaining a copy
+      of this software and associated documentation files (the "Software"), to deal
+      in the Software without restriction, including without limitation the rights
+      to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+      copies of the Software, and to permit persons to whom the Software is
+      furnished to do so, subject to the following conditions:
+      
+      The above copyright notice and this permission notice shall be included in all
+      copies or substantial portions of the Software.
+      
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+      IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+      FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+      AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+      LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+      OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+      SOFTWARE.` ; 
       break;
     case 'GNU General Public V3.0':
       section = 'This project has a license with [GNU General Public V3.0], (https://choosealicense.com/licenses/gpl-3.0/)';
